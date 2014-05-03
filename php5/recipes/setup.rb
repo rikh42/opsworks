595a7cb564e85e19e3a6e93673443bd556577a14
Rik Heywood <rik@smallneatbox.co.uk>
2014-05-03 20:22:48 +0100

    typo

--- php5/recipes/setup.rb
+++ php5/recipes/setup.rb
@@ -20,9 +20,9 @@ execute "apt-get update" do
 end
  
 # update PHP to version 5.4 and add necessary packages
-%w{php5 php5-common php-apc php5-memcached php5-memcache php5-intl php5-mysqlnd php5-curl php5-xsl php5-sqlite php5-dev php5-gd php5-cli php5-mcrpyt}.each do |pkg|
+%w{php5 php5-common php-apc php5-memcached php5-memcache php5-intl php5-mysqlnd php5-curl php5-xsl php5-sqlite php5-dev php5-gd php5-cli php5-mcrypt}.each do |pkg|
   package pkg do
-    action :install
+    action :upgrade
   end
 end
 
