a4fce498f5685cd8b28d6352374ed8dc5b67a2dc
Rik Heywood <rik@smallneatbox.co.uk>
2014-05-04 17:49:43 +0100

    trying to upgrade to just 5.4

---
+++ php5/recipes/upgrade.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: Pod Point
+# Recipe:: setup
+include_recipe "apt"
+
+
+# add repository for PHP 5.5
+apt_repository "php55" do
+  uri "http://ppa.launchpad.net/ondrej/php5-oldstable/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "E5267A6C"
+end
+ 
+# update apt info
+execute "apt-get update" do
+  command "apt-get update"
+  ignore_failure true
+end
+ 
+# update PHP to version 5.4 and add necessary packages
+%w{php5 php5-common php-apc php5-memcached php5-memcache php5-intl php5-mysqlnd php5-curl php5-xsl php5-sqlite php5-gd php5-cli php5-mcrypt}.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+
+
+
