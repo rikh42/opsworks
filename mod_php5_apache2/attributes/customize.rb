1c02e5a7b59cf4711a2f470af0eca2eb4ac4a0db
Rik Heywood <rik.heywood@pod-point.com>
2014-05-13 16:10:10 +0100

    Customise the installed php packages

---
+++ mod_php5_apache2/attributes/customize.rb
@@ -0,0 +1,20 @@
+
+
+default[:mod_php5_apache2][:packages] = [
+	'php5-xsl',
+    'php5-curl',
+    'php5-xmlrpc',
+    'php5-sqlite',
+    'php5-dev',
+    'php5-gd',
+    'php5-cli',
+    'php5-mysql',
+    'php5-mcrypt',
+    'php5-memcache',
+    'php-pear',
+    'php-xml-parser',
+    'php-mail-mime',
+    'php-db',
+    'php-mdb2',
+    'php-html-common'
+]
