3bee65a1a6fafde1809bfaa2855b40e0ab6a651a
Rik <rik@smallneatbox.co.uk>
2014-05-13 12:22:06 +0100

    Different composer method

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -2,25 +2,25 @@
 
 
 # Install Composer
-#bash "install_composer" do
-#     user "root"
-#     cwd "/tmp"
-#     code <<-EOH
-#	   curl -sS https://getcomposer.org/installer | php
-#	   chmod 755 composer.phar
-#	   mv composer.phar /usr/local/bin/composer
-#     EOH
-#     not_if "test -f /usr/local/bin/composer"
-#end
+bash "install_composer" do
+     user "root"
+     cwd "/tmp"
+     code <<-EOH
+	   curl -sS https://getcomposer.org/installer | php
+	   chmod 755 composer.phar
+	   mv composer.phar /usr/local/bin/composer
+     EOH
+     not_if "test -f /usr/local/bin/composer"
+end
 
 
 #Should really keep the composer.phar in the git repo. It simplifies this a lot
 
-remote_file "/usr/local/bin/composer" do
-    source 'http://getcomposer.org/composer.phar'
-    mode 755
-    action :create
-    not_if do
-      ::File.exist?("/usr/local/bin/composer")
-    end
-end
+#remote_file "/usr/local/bin/composer" do
+#    source 'http://getcomposer.org/composer.phar'
+#    mode 755
+#    action :create
+#    not_if do
+#      ::File.exist?("/usr/local/bin/composer")
+#    end
+#end
